e4e206676e66d80541c545a1d91dceeffab63d94
Divit Khullar <divit.khullar@nagarro.com>
2023-02-16 17:14:41 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Nextjs app deployed on Azure&nbsp;
+            Nextjs app deployed on Azure using static webApp&nbsp;
             <code className={styles.code}></code>
           </p>
           <div>
